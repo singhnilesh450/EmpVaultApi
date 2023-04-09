f6961b7356835a71d8ce0af3e09462c273f23143
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2023-04-09 13:29:18 +0530

    restricted all api endpoints based on different roles and performed api testing

--- src/main/java/com/singhfusion/EmpVault/security/ApiSecurityConfig.java
+++ src/main/java/com/singhfusion/EmpVault/security/ApiSecurityConfig.java
@@ -3,9 +3,12 @@ package com.singhfusion.EmpVault.security;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ApiSecurityConfig {
@@ -16,4 +19,24 @@ public class ApiSecurityConfig {
         UserDetails vimdhayak= User.builder().username("vimdhayakjee").password("{noop}vim123").roles("EMPLOYEE","MANAGER","ADMIN").build();
         return new InMemoryUserDetailsManager(cheems,dogelina,vimdhayak);
     }
-}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTP Basic authentication
+        http.httpBasic();
+
+        // disable Cross Site Request Forgery (CSRF)
+        // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf().disable();
+
+        return http.build();
+    }}
