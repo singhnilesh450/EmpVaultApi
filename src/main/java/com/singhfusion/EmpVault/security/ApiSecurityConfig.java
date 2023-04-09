9a77cd5e8f478a6fac53ba295dde3486180d374d
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2023-04-09 16:48:58 +0530

    password storage in db as plaintext

--- src/main/java/com/singhfusion/EmpVault/security/ApiSecurityConfig.java
+++ src/main/java/com/singhfusion/EmpVault/security/ApiSecurityConfig.java
@@ -8,16 +8,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ApiSecurityConfig {
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails cheems= User.builder().username("dogesh").password("{noop}Doge123").roles("EMPLOYEE").build();
-        UserDetails dogelina= User.builder().username("dogelina").password("{noop}dogelina123").roles("EMPLOYEE","MANAGER").build();
-        UserDetails vimdhayak= User.builder().username("vimdhayakjee").password("{noop}vim123").roles("EMPLOYEE","MANAGER","ADMIN").build();
-        return new InMemoryUserDetailsManager(cheems,dogelina,vimdhayak);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        return new JdbcUserDetailsManager(dataSource);
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
